caddb68f579e4b32e09076911aa89285364f721a
NIKHIL95969 <93199036+NIKHIL95969@users.noreply.github.com>
2022-09-20 06:54:24 +0530

    sort 0 1
    array related questions

---
+++ Array/sort01.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// function to print array
+void printarr(int *arr, int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// function to sort the arry
+void sort01(int *arr, int n){
+    int left = 0;
+    int right = n-1;
+    while (left<right)
+    {
+        if(arr[left]==0 && left < right){
+            left++;
+        }
+        if (arr[right]==1 && left < right)
+        {
+            right--;
+        }
+        if (arr[left]!=arr[right] && left < right)
+        {
+            swap(arr[left], arr[right]);
+        }
+        
+        
+    }
+    
+}
+
+int main(){
+    int arr[10] = {1,0,1,0,0,0,1,1,1,0};
+    sort01(arr, 10);
+    printarr(arr, 10);
+    return 0;
+}
