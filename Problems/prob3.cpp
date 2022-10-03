df2ad063f33f5af72157305781ed400ee75c8830
Nikhil Singh <nikhilsingh95969@gmail.com>
2022-10-03 20:15:09 +0530

    today code

--- Problems/prob3.cpp
+++ Problems/prob3.cpp
@@ -1,24 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
 
-
-void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int it:nums1){
-            //int i = (*it);
-            if((it) == 0 ){
-                nums1.erase(it);
-            }
-        }
-        for(int i=nums1.size()-1; i>=0; i--){
-            if(nums1[i]==0){
-                nums1.pop_back();
-            }
-        }
-        for(int i=0; i<n; i++){
-            nums1.push_back(nums2[i]);
-        }
+// void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         for(int it:nums1){
+//             //int i = (*it);
+//             if((it) == 0 ){
+//                 nums1.erase(it);
+//             }
+//         }
+//         for(int i=nums1.size()-1; i>=0; i--){
+//             if(nums1[i]==0){
+//                 nums1.pop_back();
+//             }
+//         }
+//         for(int i=0; i<n; i++){
+//             nums1.push_back(nums2[i]);
+//         }
         
-        sort(nums1.begin(), nums1.end());
+//         sort(nums1.begin(), nums1.end());
     
-}
+// }
 
 int main(){
 
@@ -31,7 +32,26 @@ int main(){
        cin >> a;
        nums.push_back(a);
     }
-    sort(nums.end(), nums.begin());
+    //sort(nums.end(), nums.begin());
+    //vector<int>::iterator it;
+   vector<int>::iterator it;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << " ";
+        it = nums.begin()+i;
+        if (*it == 0)
+        {
+            nums.erase(it);
+            i--;
+        }
+        
+    }
+    
+    // for (it=nums.begin(); it!=nums.end()-2; it++)
+    // {
+        
+    // }
+    
     for (int i = 0; i < nums.size(); i++)
     {
         cout << nums[i] <<" ";
