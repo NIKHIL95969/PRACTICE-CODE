dacad66ef0d8ab106a8fc7569de41a214a43d126
Nikhil Singh <93199036+NIKHIL95969@users.noreply.github.com>
2022-11-20 16:09:01 +0530

    commit line

--- Linked List/removeDuplicates.cpp
+++ Linked List/removeDuplicates.cpp
@@ -5,7 +5,7 @@ Node* removeDuplicates(Node* head)
         Node* temp = head;
         
         while( temp!=NULL && temp->next!=NULL){
-            cout << temp->val << " - " << temp->next->val << endl;
+            //cout << temp->val << " - " << temp->next->val << endl;
             if(temp->val==temp->next->val){
                 Node* nextN = temp->next;
                 temp->next = nextN->next;
