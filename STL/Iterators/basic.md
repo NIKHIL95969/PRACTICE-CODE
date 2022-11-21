fd525dcc8724fd9d61fedde3ccb104f4bad74718
Nikhil Singh <nikhilsingh95969@gmail.com>
2022-11-21 22:18:00 +0530

    write about iterators

---
+++ STL/Iterators/basic.md
@@ -0,0 +1,8 @@
+# ITERATORS
+Iterators are used to point at the memory location.
+
+
+
+### NOTE: 
+- If **it** is a iterator and we use **it++** and **it+1** in array point to the next address. As we now that the data stored in array is in continuous order so it work.
+- But in map or other datatype the data may be not stored in continuous order in that case we use **it++** that point to the next iterator location but the **it+1** point to the next location which is invalid.
--- Sorting/quickSort.cpp
+++ Sorting/quickSort.cpp
@@ -14,16 +14,20 @@ int partion(int arr[], int  s, int e){
     int i =s, j =e;
     while (i<p_index && j>p_index)
     {
+        // cout << arr[i] << " " << arr[j] << endl;
         while (arr[i]<=pivot)
         {
             i++;
         }
         while (arr[j]>pivot)
         {
+            // cout << arr[j] <<" j " ;
             j++;
+            //cout << arr[j] << endl ;
         }
         if (i<p_index && j>p_index) 
         {
+            // cout << i << " c " << j << endl;
             swap(arr[i++],arr[j--]);
         }
         
@@ -35,13 +39,14 @@ void quickSort(int arr[], int s, int e){
     if(s>=e) return;
 
     int p = partion(arr,s,e);
+    
 
     quickSort(arr,s,p-1);
     quickSort(arr, p+1, e);
 }
 
 int main(){
-    int arr[] = {3,1,2,4,5,99,46,0,2};
+    int arr[] = {3,1,2,0,2,4,};
     int n = sizeof(arr)/sizeof(arr[0]);
     quickSort(arr, 0 ,n-1);
     cout << "Done" << endl;
