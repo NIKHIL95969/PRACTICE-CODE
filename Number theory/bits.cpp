d78d0d4e8dcce7168284ae2bb959e112eaa27524
Nikhil Singh <nikhilsingh95969@gmail.com>
2022-12-09 21:48:04 +0530

    about set bits

--- Number theory/bits.cpp
+++ Number theory/bits.cpp
@@ -1,16 +1,3 @@
-### Modular Arithmetic
-
-- (a+b)%M  = ((a%M) + (b%M))%M
-- (a*b)%M  = ((a%M) * (b%M))%M
-- (a-b)%M  = ((a%M) - (b%M) + M )%M
-- (a/b)%M  = ((a%M) * (b^-1) % M )%M
-
-## Why we need modular arithmetic ?
-
-- Help to safe form overflow 
-
-
-```
 #include<bits/stdc++.h>
 using namespace std;
 void print(int n){
@@ -28,5 +15,3 @@ int main(){
     else cout << "\nNot set bit";
     return 0;
 }
-
-```
