4083cf11e11e6c8a5d4cdfb257b99a02c6e74c1a
Nikhil Singh <nikhilsingh95969@gmail.com>
2023-01-30 15:21:18 +0530

    questions

--- Array/tempCodeRunnerFile.cpp
+++ Array/tempCodeRunnerFile.cpp
@@ -1,18 +1,3 @@
-for(int i = 0; i<prices.size()-1; i++){
-    //     if(minPrice>prices[i]){
-    //         minPrice = prices[i];
-    //         minIndex = i;
-    //     }
-    // }
-    // cout << "Min index " << minIndex << endl;
-    // if(minIndex == (prices.size()-1)){
-    //     return 0;
-    // }
-    // int profit = 0;
-    // for(int i = minIndex+1; i<prices.size(); i++){
-    //     if(minPrice <= prices[i]){
-            
-    //         profit = max(profit, (prices[i]-minPrice));
-    //     }
-    // }
-    // return profit;
+int h = min(arr[i],arr[j]);
+        // int b = j-i;
+        // ans = max(ans, h*b);
